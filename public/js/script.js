591c196e1c368ff747303e66b9e1f801f969f7e4
Mateusz Będkowski <mateusz.bedkowski@op.pl>
2022-05-07 20:21:39 +0200

    TrainWise

---
+++ public/js/script.js
@@ -0,0 +1,29 @@
+const footerYear = document.querySelector('.footer__year');
+
+document.addEventListener('DOMContentLoaded', function () {
+	const nav = document.querySelector('.navbar');
+	const allNavItems = document.querySelectorAll('.nav-link');
+	const navList = document.querySelector('.navbar-collapse');
+
+	function addShadow() {
+		if (window.scrollY >= 300) {
+			nav.classList.add('shadow-bg');
+		} else {
+			nav.classList.remove('shadow-bg');
+		}
+	}
+
+	allNavItems.forEach((item) =>
+		item.addEventListener('click', () => navList.classList.remove('show'))
+	);
+
+	window.addEventListener('scroll', addShadow);
+});
+
+const handleCurrentYear = () => {
+    const year = (new Date).getFullYear();
+    footerYear.innerText = year;
+}
+
+handleCurrentYear();
+function navClick() {}
